992b2446d95cdf88c6356fe472887bf5bb295589
Brian Demers <bdemers@apache.org>
2020-08-22 11:55:43 -0400

    moving site publishing to git

---
+++ assets/js/shiro-site.js
@@ -0,0 +1,16 @@
+
+function addPageEditLink() {
+
+    var ghRepo = "apache/shiro-site";
+    var ghRelHref = "https://github.com/" + ghRepo + "/edit/master/";
+
+    var ghEditPageElement = $( "#ghEditPage" );
+    var editThisPageElement = $(".editThisPage");
+
+    if (ghEditPageElement && ghEditPageElement.val() && editThisPageElement) {
+        var relGHPage = ghEditPageElement.val();
+
+        var ghEditHref = ghRelHref + relGHPage;
+        editThisPageElement.append("<a href='" + ghEditHref + "'>Edit this Page on Github</a>");
+    }
+}
---
+++ static/1.6.0/shiro-event/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.apache.shiro.event"},{"l":"org.apache.shiro.event.support"}]
