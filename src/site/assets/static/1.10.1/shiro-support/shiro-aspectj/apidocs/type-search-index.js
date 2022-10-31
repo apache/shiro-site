9c3b23a2e142274a482570e27ed44a9db8e170de
Benjamin Marwell <bmarwell@apache.org>
2022-10-31 22:24:52 +0100

    [BLOG] prepare v1.10.1 announcement.

---
+++ src/site/assets/static/1.10.1/shiro-support/shiro-aspectj/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.apache.shiro.aspectj","l":"AspectjAnnotationsAuthorizingMethodInterceptor"},{"p":"org.apache.shiro.aspectj","l":"BeforeAdviceMethodInvocationAdapter"},{"p":"org.apache.shiro.aspectj","l":"ShiroAnnotationAuthorizingAspect"}]
