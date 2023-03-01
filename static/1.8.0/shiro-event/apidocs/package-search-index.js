206b06f9512cdd7cf0ec501a910606d7e6be32dd
lprimak <lenny@flowlogix.com>
2023-02-28 21:46:00 -0500

    update site 2023-02-28, old static sites removed

--- static/1.8.0/shiro-event/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.apache.shiro.event"},{"l":"org.apache.shiro.event.support"}]
--- static/1.9.0/shiro-support/shiro-hazelcast/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.apache.shiro.hazelcast.cache","l":"HazelcastCacheManager"}]
